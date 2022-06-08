37cd7008941eff96bd0ec1a1548f5b13e9122027
Hoa-Binh Phan <hoa-binhphan@Hoa-Binhs-MacBook-Pro.local>
2022-06-08 13:46:00 +0200

    version1

---
+++ src/Ray.java
@@ -0,0 +1,79 @@
+public class Ray {
+    Vec3 origin;
+    Vec3 direction;
+    double t;  //Abstand bis man irgendwas trifft
+
+    public Ray() {
+    }
+
+    public Ray(Vec3 a, Vec3 b) {
+        origin = a;
+        direction  = b;
+        t = 0;
+    }
+
+    public Ray(Vec3 a, Vec3 b, double ti) {
+        origin = a;
+        direction = b;
+        t = ti;
+    }
+
+    public Vec3 origin() {
+        return origin;
+    }
+
+    public Vec3 direction() {
+        return direction;
+    }
+
+    public Vec3 at(double t){
+        return origin.add(direction.mul(t));
+    }
+
+    public double time() {
+        return t;
+    }
+
+    public Vec3 point_at_parameter(double t) {
+        return origin.add(direction.mul(t));
+    }
+
+    public void set(Ray r) {
+        origin = r.origin;
+        direction = r.direction;
+        t = r.t;
+    }
+
+    public Vec3 color(Ray r){
+        //if(hit_sphere(new Vec3(0,0,-1),0.5, r)){
+           //return new Vec3(1,0,0);
+        //}
+        double t = hit_sphere(new Vec3(0,0,-1), 0.5, r);
+        if(t>0.0){
+            Vec3 N = r.at(t).sub(new Vec3(0,0,-1)).unit_vector();
+            return new Vec3(N.x()+1, N.y()+1, N.z()+1).mul(0.5);
+        }
+        Vec3 unit_direction = this.direction().unit_vector();  //fest Länge Einheitsvektor
+        t = (float) (0.5*(unit_direction.y()+1.0));
+        return new Vec3(1.0,1.0,1.0).mul(1.0-t).add(new Vec3(0.5,0.7,1.0).mul(t));
+
+    }
+
+    public double hit_sphere(Vec3 center, double radius, Ray r ){
+        Vec3 oc = r.origin().sub(center);
+        double a = Vec3.dot(r.direction(), r.direction());
+        double b = 2.0 * (Vec3.dot(oc, r.direction()));
+        double c = Vec3.dot(oc,oc) - (radius*radius);
+        double discriminant = b*b - 4*a*c;
+        //return (discriminant > 0);
+        if(discriminant<0){
+            return -1.0;
+        } else{
+            return (-b - Math.sqrt(discriminant))/(2.0*a);
+        }
+
+        
+
+    }
+}
+
---
+++ src/Vec3.java
@@ -0,0 +1,104 @@
+public class Vec3 {
+    private double [] e = new double [3];
+
+    public Vec3(double e0, double e1, double e2) {
+        e[0] = e0;
+        e[1] = e1;
+        e[2] = e2;
+    }
+
+    public double x(){return e[0]; }
+    public double y(){return e[1]; }
+    public double z(){return e[2]; }
+
+    public double r(){return e[0]; }
+    public double g(){return e[1]; }
+    public double b(){return e[2]; }
+
+    public double length(){
+        return Math.sqrt(e[0]*e[0]+e[1]*e[1]+e[2]*e[2]);}
+
+    public double squared_length(){
+        return e[0]*e[0]+e[1]*e[1]+e[2]*e[2];}
+
+
+
+    public  Vec3 minus(){
+        return  new Vec3(-e[0],-e[1],-e[2]);
+    }
+    public Vec3 add(Vec3 other){
+        return new Vec3(e[0]+ other.e[0], e[1] + other.e[1], e[2] + other.e[2]);
+    }
+
+    public Vec3 sub(Vec3 other){
+        return new Vec3(e[0] - other.e[0], e[1] - other.e[1], e[2] - other.e[2]);
+    }
+    public Vec3 mul(Vec3 other){
+        return new Vec3(e[0] * other.e[0], e[1] * other.e[1], e[2] * other.e[2]);
+    }
+    public Vec3 mul(double t){
+        return new Vec3(t*e[0],t*e[1],t*e[2]);
+    }
+
+    public Vec3 div(Vec3 other){
+        return new Vec3(e[0] / other.e[0], e[1] / other.e[1], e[2] / other.e[2]);
+    }
+    public Vec3 div(double t){
+        return new Vec3(e[0]/t,e[1]/t,e[2]/t);
+    }
+
+    public static double dot(Vec3 v1, Vec3 v2){
+        return v1.e[0]*v2.e[0] + v1.e[1]*v2.e[1] + v1.e[2]*v2.e[2];
+    }
+
+    public static  Vec3 cross(Vec3 v1, Vec3 v2){
+        return new Vec3((v1.e[1]*v2.e[2] - v1.e[2]*v2.e[1]),(-(v1.e[0]*v2.e[2] - v1.e[2]*v2.e[0])
+                ),(v1.e[0]*v2.e[1] - v1.e[1]*v2.e[0]));
+    }
+
+    public void unit(){
+        double k = 1.0 / length();
+        e[0]*=k;
+        e[1]*=k;
+        e[2]*=k;
+    }
+
+    public Vec3 unit_vector(){
+
+        return this.div(this.length());
+    }
+
+    public void set(Vec3 v){
+        e[0] = v.e[0];
+        e[1] = v.e[1];
+        e[2] = v.e[2];
+    }
+
+    public double get(int i){
+        return e[i];
+    }
+
+    public String toString(){
+        return "<" + e[0] + "," + e[1]  + "," + e[2] + ">";
+    }
+
+    public boolean equals(Vec3 v2){
+        return e[0] == v2.e[0] && e[1] == v2.e[1] && e[2] ==  v2.e[2];
+    }
+
+    public void write_color(Vec3 pixel_color){
+
+        int ir = (int) (255.999 * pixel_color.r());
+        int ig = (int) (255.999 * pixel_color.g());
+        int ib = (int) (255.999 * pixel_color.b());
+        System.out.println(ir + " " + ig + " " + ib + " " + "\n");
+    }
+
+
+
+
+
+}
+
+
+
