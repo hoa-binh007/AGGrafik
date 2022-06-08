ed1fc109d193e134e62c21a75fb89c6d11aec70e
Hoa-Binh Phan <hoa-binhphan@Hoa-Binhs-MacBook-Pro.local>
2022-06-08 13:52:21 +0200

    Diskreminante geändert 4 auf 3

--- src/Ray.java
+++ src/Ray.java
@@ -64,7 +64,7 @@ public class Ray {
         double a = Vec3.dot(r.direction(), r.direction());
         double b = 2.0 * (Vec3.dot(oc, r.direction()));
         double c = Vec3.dot(oc,oc) - (radius*radius);
-        double discriminant = b*b - 4*a*c;
+        double discriminant = b*b - 3*a*c;
         //return (discriminant > 0);
         if(discriminant<0){
             return -1.0;
