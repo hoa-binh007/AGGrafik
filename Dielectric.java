47d53e3358c92636677ec2281375447e45f36440
hoa-binh007 <104062106+hoa-binh007@users.noreply.github.com>
2022-09-09 15:28:16 +0200

    Add files via upload
    That definitely doesn't look right. One troublesome practical issue is that when the ray is in the material with the higher refractive index, there is no real solution to Snell’s law, and thus there is no refraction possible. If we refer back to Snell's law and the derivation of sin𝜃′
    sin
    ⁡
    θ
    ′
    :
    
    sin𝜃′=𝜂𝜂′⋅sin𝜃
    sin
    ⁡
    θ
    ′
    =
    η
    η
    ′
    ⋅
    sin
    ⁡
    θ
    
    If the ray is inside glass and outside is air (𝜂=1.5
    η
    =
    1.5
     and 𝜂′=1.0
    η
    ′
    =
    1.0
    ):
    
    sin𝜃′=1.51.0⋅sin𝜃
    sin
    ⁡
    θ
    ′
    =
    1.5
    1.0
    ⋅
    sin
    ⁡
    θ
    
    The value of sin𝜃′
    sin
    ⁡
    θ
    ′
     cannot be greater than 1. So, if,
    
    1.51.0⋅sin𝜃>1.0
    1.5
    1.0
    ⋅
    sin
    ⁡
    θ
    >
    1.0
    ,
    
    the equality between the two sides of the equation is broken, and a solution cannot exist. If a solution does not exist, the glass cannot refract, and therefore must reflect the ray:
    
    if (refraction_ratio * sin_theta > 1.0) {
        // Must Reflect
        ...
    } else {
        // Can Refract
        ...
    }
    
    Here all the light is reflected, and because in practice that is usually inside solid objects, it is called “total internal reflection”. This is why sometimes the water-air boundary acts as a perfect mirror when you are submerged.
    
    We can solve for sin_theta using the trigonometric qualities:
    sin𝜃=1−cos2𝜃‾‾‾‾‾‾‾‾‾‾√
    sin
    ⁡
    θ
    =
    1
    −
    cos
    2
    ⁡
    θ
    
    and
    
    cos𝜃=𝐑⋅𝐧
    cos
    ⁡
    θ
    =
    R
    ⋅
    n
    
    double cos_theta = fmin(dot(-unit_direction, rec.normal), 1.0);
    double sin_theta = sqrt(1.0 - cos_theta*cos_theta);
    
    if (refraction_ratio * sin_theta > 1.0) {
        // Must Reflect
        ...
    } else {
        // Can Refract
        ...
    }
    Listing 57: [material.h] Determining if the ray can refract

--- Dielectric.java
+++ Dielectric.java
@@ -8,7 +8,7 @@ public class Dielectric implements Material{
 
     @Override
     public boolean scatter(Ray r_in, Hit_record rec, Vec3 attenuation, Ray scattered) {
-        new Vec3(1.0, 1.0, 1.0);
+        attenuation = new Vec3(1.0,1.0,1.0);
         double refraction_ratio;
         if(rec.front_face) {
             refraction_ratio = 1.0 / ir;
@@ -19,10 +19,27 @@ public class Dielectric implements Material{
         }
 
         Vec3 unit_direction = Vec3.unit_vector(r_in.direction());
-        Vec3 refracted = Vec3.refract(unit_direction,rec.normal,refraction_ratio);
-        scattered.origin = rec.p;
-        scattered.direction = refracted;
+
+        double cos_theta = Math.min(Vec3.dot(unit_direction.mul(-1),rec.normal),1.0);
+        double sin_theta = Math.sqrt(1.0-cos_theta*cos_theta) ;
+        boolean cannot_refract = refraction_ratio * sin_theta > 1.0;
+        Vec3 direction;
+        if(cannot_refract){
+            direction = Lambertian.reflect(unit_direction, rec.normal);
+        }
+        else {
+            direction = Vec3.refract(unit_direction, rec.normal, refraction_ratio);
+            scattered.origin = rec.p;
+            scattered.direction = direction;
+        }
         return true;
+
+
+        //Vec3 refracted = Vec3.refract(unit_direction,rec.normal,refraction_ratio);
+        //scattered.origin = rec.p;
+        //scattered.direction = refracted;
+        //return true;
+
 
     }
 }
